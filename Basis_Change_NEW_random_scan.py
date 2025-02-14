some minor changes, corrected chisq calculation w different upper and lower error

Basis_Change_NEW_random_scan.py
@@ -11,30 +11,33 @@ def random_bp_generation():
     # TODO this part has to be adapted: define the ranges of variation below
     # NOTE currently the ch1tt and ch1bb checks are turned off
     # choose a bp by random selection of values in the ranges given above
-    mAS = 1000 #random_number_gen_w_bounds(np.array([70-5, 70+5]))
+    mAS = 390 #random_number_gen_w_bounds(np.array([70-5, 70+5]))
     mh1 = 800 #random_number_gen_w_bounds(np.array([95.4, 95.4]))
     mh2 = 125.09 #random_number_gen_w_bounds(np.array([125.09, 125.09]))
-    mh3 = 2900 #random_number_gen_w_bounds(np.array([150-5, 150+15])) #np.array([2*mAS, 1200])
+    mh3 = 900 #random_number_gen_w_bounds(np.array([150-5, 150+15])) #np.array([2*mAS, 1200])
     mA = 800 #random_number_gen_w_bounds(np.array([900, 900])) #np.array([mh3-50, mh3+50])
     mHm = 800 #random_number_gen_w_bounds(np.array([900, 900])) #np.array([mh3-50, mh3+50])
     v = 246.220569 #random_number_gen_w_bounds(np.array([246.220569, 246.220569]))
-    vS = random_number_gen_w_bounds(np.array([100, 5000]))
-    tanbeta = random_number_gen_w_bounds(np.array([1, 10]))
-    #ch1bb = random_number_gen_w_bounds(np.array([-1,1])) #[0, 0.581]))
-    #ch1tt = random_number_gen_w_bounds(np.array([-1,1])) #[np.max([0.267, ch1bb]), 0.583])) #np.array([0.267, 0.583])
-    mutil2 = random_number_gen_w_bounds(np.array([670000-300000, 670000+200000]))
+    tanbeta = random_number_gen_w_bounds(np.array([2.130902-0.5, 2.130902+0.5])) #2.130902
+    #ch1bb = random_number_gen_w_bounds(np.array([-1, 1])) #[0, 0.581]))
+    #ch1tt = random_number_gen_w_bounds(np.array([-1, 1])) #[np.max([0.267, ch1bb]), 0.583])) #np.array([0.267, 0.583])
+    mutil2_low, mutil2_up = get_range_for_mutil2_red(v, mA, mHm)
+    mutil2 = random_number_gen_w_bounds(np.array([mutil2_low, mutil2_up]))
     #mSp2 = random_number_gen_w_bounds(np.array([-80000, 600000]))
-    #alignm = #random_number_gen_w_bounds(np.array([0.998, 1]))
-    l1ml3pp = random_number_gen_w_bounds(np.array([-3, 3]))
-    l1m24p = random_number_gen_w_bounds(np.array([-3, 3]))
-    l2m25p = random_number_gen_w_bounds(np.array([-3, 3]))
+    #alignm = random_number_gen_w_bounds(np.array([0.998, 1]))
+    l1ml3pp = random_number_gen_w_bounds(np.array([-0.427248-0.01, -0.427248+0.02])) #-0.427248
+    l1m24p = random_number_gen_w_bounds(np.array([0.077784-0.01, 0.077784+0.08])) #0.077784
+    l2m25p = random_number_gen_w_bounds(np.array([0.036923-0.08, 0.036923+0.01])) #0.036923
     #lh1 = random_number_gen_w_bounds(np.array([-3, 3]))
     #lh2 = random_number_gen_w_bounds(np.array([-3, 3]))
-    a1 = random_number_gen_w_bounds(np.array([-np.pi, np.pi]))
-    a2 = random_number_gen_w_bounds(np.array([-np.pi, np.pi]))
-    a3 = random_number_gen_w_bounds(np.array([-np.pi, np.pi]))
-    #cosbeta_a1 = random_number_gen_w_bounds(np.array([-0.2, 0.2]))
+    a1 = random_number_gen_w_bounds(np.array([-0.5, -0.2])) #-0.421942
+    a2 = random_number_gen_w_bounds(np.array([-0.01, 0.02])) #-0.014232
+    a3 = random_number_gen_w_bounds(np.array([-0.02, 0.01])) #-0.007314
+    #cosbeta_a1 = random_number_gen_w_bounds(np.array([-1, 1])) # cos(beta-a1)
     #a1 = -np.arccos(cosbeta_a1) + np.arctan(tanbeta)
+    #vS_low, vS_up = get_range_for_vS_red_2(mh1, mh2, mh3, a1, a2, a3, l1ml3pp)
+    #vS = random_number_gen_w_bounds(np.array([vS_low, vS_up]))
+    vS = random_number_gen_w_bounds(np.array([587.168152-50, 587.168152+50])) #587.168152
 
     # put into a data frame
     #data = np.array([[mh1, mh2, mh3, mA, mAS, mHm, v, vS, tanbeta,
@@ -68,10 +71,112 @@ def random_number_gen_w_bounds(range_ar: np.ndarray):
     random_number_in_range = lower_bound + np.random.rand()*(upper_bound - lower_bound)
     return random_number_in_range
 
+def get_range_for_mutil2_full(tanbeta, v, mh1, mh2, mh3, mA, mHm):
+    # we want the interaction basis parameters
+    # to be of order 1
+    # we can solve e.g. l1 for mutil2
+    # for simplicity we use the same constant factor for all entries
+    # of the scalar mixing matrix
+    factor = 0.25
+    beta = np.arctan(tanbeta)
+    mutil2_bound1 = (v**2 * np.cos(beta)**2 - factor*(mh1**2 + mh2**2 + mh3**2))*(-1/(np.sin(beta)**2))
+    # we can also solve e.g. l2 for mutil2
+    mutil2_bound2 = (v**2 * np.sin(beta)**2 - factor*(mh1**2 + mh2**2 + mh3**2))*(-1/(np.cos(beta)**2))
+    # we can also solve e.g. l3 for mutil2
+    mutil2_bound3 = (-1)*(v**2 - 2*mHm**2 - (1/np.sin(beta)/np.cos(beta))*factor*(mh1**2 + mh2**2 + mh3**2))
+    # we can also solve e.g. l4 for mutil2
+    mutil2_bound4 = v**2 + 2*mHm**2 - mA**2
+    # we can also solve e.g. l5 for mutil2
+    mutil2_bound5 = v**2 + mA**2
+    print(mutil2_bound1)
+    print(mutil2_bound2)
+    print(mutil2_bound3)
+    print(mutil2_bound4)
+    print(mutil2_bound5)
+    # we calculate the mean of the bounds
+    mutil2_bound_mean = np.mean([mutil2_bound1, mutil2_bound2, mutil2_bound3, mutil2_bound4, mutil2_bound5])
+    return mutil2_bound_mean
 
-if __name__=='__main__':
+def get_range_for_mutil2_red(v, mA, mHm):
+    # we want the interaction basis parameters
+    # to be of order 1 (and not larger than 4*pi)
+    # we can solve e.g. l4 for mutil2
+    mutil2_bound4_low = -4*np.pi*v**2 + 2*mHm**2 - mA**2
+    mutil2_bound4_up = 4*np.pi*v**2 + 2*mHm**2 - mA**2
+    # we can also solve e.g. l5 for mutil2
+    mutil2_bound5_low = -4*np.pi*v**2 + mA**2
+    mutil2_bound5_up = 4*np.pi*v**2 + mA**2
+    # we calculate the mean of the bounds
+    mutil2_bound_low = np.max([mutil2_bound4_low, mutil2_bound5_low])
+    mutil2_bound_up = np.min([mutil2_bound4_up, mutil2_bound5_up])
+    return mutil2_bound_low, mutil2_bound_up
+
+def get_range_for_vS_red(tanbeta, v, mh1, mh2, mh3, l1m24p, l2m25p, a1, a2, a3):
+    # NOTE this functions does not seem to work properly, maybe there is a typo
+    # we want the interaction basis parameters
+    # to be of order 1 (and not larger than 4*pi)
+    beta = np.arctan(tanbeta)
+    R = basis_change.calc_R(a1, a2, a3)
+    R11 = R[0,0]
+    R12 = R[0,1]
+    R13 = R[0,2]
+    R21 = R[1,0]
+    R22 = R[1,1]
+    R23 = R[1,2]
+    R31 = R[2,0]
+    R32 = R[2,1]
+    R33 = R[2,2]
+    # we can solve e.g. l1p for vS
+    vS_bound_1p_1 = (mh1**2 * R11*R13 + mh2**2 * R21*R23 + mh3**2 * R31*R33) / ((2*4*np.pi - l1m24p) * v * np.cos(beta))
+    vS_bound_1p_2 = (mh1**2 * R11*R13 + mh2**2 * R21*R23 + mh3**2 * R31*R33) / ((-2*4*np.pi - l1m24p) * v * np.cos(beta))
+    vS_bound_1p_low = np.min([vS_bound_1p_1, vS_bound_1p_2])
+    vS_bound_1p_low = np.max([vS_bound_1p_low, 0])
+    vS_bound_1p_up = np.max([vS_bound_1p_1, vS_bound_1p_2])
+    # we can solve e.g. l2p for vS
+    vS_bound_2p_1 = (mh1**2 * R12*R13 + mh2**2 * R22*R23 + mh3**2 * R32*R33) / ((2*4*np.pi - l2m25p) * v * np.sin(beta))
+    vS_bound_2p_2 = (mh1**2 * R12*R13 + mh2**2 * R22*R23 + mh3**2 * R32*R33) / ((-2*4*np.pi - l2m25p) * v * np.sin(beta))
+    vS_bound_2p_low = np.min([vS_bound_2p_1, vS_bound_2p_2])
+    vS_bound_2p_low = np.max([vS_bound_2p_low, 0])
+    vS_bound_2p_up = np.max([vS_bound_2p_1, vS_bound_2p_2])
+    # we can solve e.g. l4p for vS
+    vS_bound_4p_1 = (mh1**2 * R11*R13 + mh2**2 * R21*R23 + mh3**2 * R31*R33) / ((4*4*np.pi + l1m24p) * v * np.cos(beta))
+    vS_bound_4p_2 = (mh1**2 * R11*R13 + mh2**2 * R21*R23 + mh3**2 * R31*R33) / ((-4*4*np.pi + l1m24p) * v * np.cos(beta))
+    vS_bound_4p_low = np.min([vS_bound_4p_1, vS_bound_4p_2])
+    vS_bound_4p_low = np.max([vS_bound_4p_low, 0])
+    vS_bound_4p_up = np.max([vS_bound_4p_1, vS_bound_4p_2])
+    # we can solve e.g. l5p for vS
+    vS_bound_5p_1 = (mh1**2 * R12*R13 + mh2**2 * R22*R23 + mh3**2 * R32*R33) / ((4*4*np.pi + l2m25p) * v * np.sin(beta))
+    vS_bound_5p_2 = (mh1**2 * R12*R13 + mh2**2 * R22*R23 + mh3**2 * R32*R33) / ((-4*4*np.pi + l2m25p) * v * np.sin(beta))
+    vS_bound_5p_low = np.min([vS_bound_5p_1, vS_bound_5p_2])
+    vS_bound_5p_low = np.max([vS_bound_5p_low, 0])
+    vS_bound_5p_up = np.max([vS_bound_5p_1, vS_bound_5p_2])
+    # we use the most stringent bounds
+    vS_bound_low = np.max([vS_bound_1p_low, vS_bound_2p_low, vS_bound_4p_low, vS_bound_5p_low])
+    vS_bound_up = np.min([vS_bound_1p_up, vS_bound_2p_up, vS_bound_4p_up, vS_bound_5p_up])
+    return vS_bound_low, vS_bound_up
+
+def get_range_for_vS_red_2(mh1, mh2, mh3, a1, a2, a3, l1ml3pp):
+    # we want the interaction basis parameters
+    # to be of order 1 (and not larger than 4*pi)
+    R = basis_change.calc_R(a1, a2, a3)
+    R13 = R[0,2]
+    R23 = R[1,2]
+    R33 = R[2,2]
+    # we can solve e.g. l1pp for vS
+    vS_bound_sq_1pp_1 = (mh1**2*R13**2 + mh2**2*R23**2 + mh3**2*R33**2)/(4*4*np.pi/3 - l1ml3pp/2)
+    vS_bound_sq_1pp_2 = (mh1**2*R13**2 + mh2**2*R23**2 + mh3**2*R33**2)/(-4*4*np.pi/3 - l1ml3pp/2)
+    vS_bound_sq_1pp_low = np.max([vS_bound_sq_1pp_1, vS_bound_sq_1pp_2])
+    vS_bound_1pp_low = np.sqrt(vS_bound_sq_1pp_low)
+    vS_bound_1pp_up = vS_bound_1pp_low * 5
+    return vS_bound_1pp_low, vS_bound_1pp_up
+
+def main():
     FILE_OUT_inte_b = "output/inte_basis.csv"
     FILE_OUT_mass_b = "output/mass_basis.csv"
     mass_b = random_bp_generation()
     basis_change.save_csv(FILE_OUT_mass_b, mass_b)
     basis_change.main_func(mass_b, FILE_OUT_inte_b)
+    return
+
+if __name__=='__main__':
+    main()

Get_exp_constr_rescaled_NEW.py
@@ -33,108 +33,159 @@ def get_results(data):
     Returns:
         results (dict): a dictionary containing the results
     """
-    # get retults from HiggsTools
-    hbResult, hsChisq, hsChisq_red = get_higgstools(data)
-    # calculate Chi_sq for signal from CMS and LEP
-    Chisq_CMS_LEP, mu_the_LEP, mu_the_CMS = calc_chisq_cms_lep(data)
-    # check other experimental constraints (from Planck, LZ, Fermi, ...)
-    LZconstr_pb = get_interp_constr(data["mAS"][0], constr_dict['PCS_pb'][0]) * constr_dict['PCS_pb'][1]
-    FERMIconstr_bb = get_interp_constr(data["mAS"][0], constr_dict['INDDCS_bb'][0])
-    FERMIconstr_tautau = get_interp_constr(data["mAS"][0], constr_dict['INDDCS_tautau'][0])
-    FERMIconstr_WW = get_interp_constr(data["mAS"][0], constr_dict['INDDCS_WW'][0])
-    FERMIconstr_cc = get_interp_constr(data["mAS"][0], constr_dict['INDDCS_cc'][0])
-    FERMIconstr_ee = get_interp_constr(data["mAS"][0], constr_dict['INDDCS_ee'][0])
-    FERMIconstr_yy = get_interp_constr(data["mAS"][0], constr_dict['INDDCS_yy'][0])
-    FERMIconstr_gg = get_interp_constr(data["mAS"][0], constr_dict['INDDCS_gg'][0])
-    FERMIconstr_hh = get_interp_constr(data["mAS"][0], constr_dict['INDDCS_h2h2'][0])
-    FERMIconstr_mumu = get_interp_constr(data["mAS"][0], constr_dict['INDDCS_mumu'][0])
-    FERMIconstr_qq = get_interp_constr(data["mAS"][0], constr_dict['INDDCS_qq'][0])
-    FERMIconstr_tt = get_interp_constr(data["mAS"][0], constr_dict['INDDCS_tt'][0])
-    FERMIconstr_ZZ = get_interp_constr(data["mAS"][0], constr_dict['INDDCS_ZZ'][0])
-    PLconstr = 0.1202 # 0.1191 +- 0.0009 (used a slightly hihger bound 0.1192 +- 0.0010 here)
+    # sometimes there can be values missing, account for these
+    try:
+        # get retults from HiggsTools
+        hbResult, hsChisq, hsChisq_red = get_higgstools(data)
+        HBallowed = hbResult.allowed
+        # calculate Chi_sq for signal from CMS and LEP
+        Chisq_CMS_LEP, mu_the_LEP, mu_the_CMS = calc_chisq_cms_lep(data)
+        # check other experimental constraints (from Planck, LZ, Fermi, ...)
+        LZconstr_pb = get_interp_constr(data["mAS"][0], constr_dict['PCS_pb'][0]) * constr_dict['PCS_pb'][1]
+        FERMIconstr_bb = get_interp_constr(data["mAS"][0], constr_dict['INDDCS_bb'][0])
+        FERMIconstr_tautau = get_interp_constr(data["mAS"][0], constr_dict['INDDCS_tautau'][0])
+        FERMIconstr_WW = get_interp_constr(data["mAS"][0], constr_dict['INDDCS_WW'][0])
+        FERMIconstr_cc = get_interp_constr(data["mAS"][0], constr_dict['INDDCS_cc'][0])
+        FERMIconstr_ee = get_interp_constr(data["mAS"][0], constr_dict['INDDCS_ee'][0])
+        FERMIconstr_yy = get_interp_constr(data["mAS"][0], constr_dict['INDDCS_yy'][0])
+        FERMIconstr_gg = get_interp_constr(data["mAS"][0], constr_dict['INDDCS_gg'][0])
+        FERMIconstr_hh = get_interp_constr(data["mAS"][0], constr_dict['INDDCS_h2h2'][0])
+        FERMIconstr_mumu = get_interp_constr(data["mAS"][0], constr_dict['INDDCS_mumu'][0])
+        FERMIconstr_qq = get_interp_constr(data["mAS"][0], constr_dict['INDDCS_qq'][0])
+        FERMIconstr_tt = get_interp_constr(data["mAS"][0], constr_dict['INDDCS_tt'][0])
+        FERMIconstr_ZZ = get_interp_constr(data["mAS"][0], constr_dict['INDDCS_ZZ'][0])
+        PLconstr = 0.1202 # 0.1191 +- 0.0009 (used a slightly hihger bound 0.1192 +- 0.0010 here)
 
-    # calculate rescaled parameters
-    Rel_f = data["RelDen"][0]/PLconstr
-    PCS_res_pb = data["PCS_pb"][0]*Rel_f
-    NCS_res_pb = data["NCS_pb"][0]*Rel_f
-    INDDCS_res_cm3_over_s = data["INDDCS_cm3_over_s"][0]*Rel_f**2
+        # calculate rescaled parameters
+        Rel_f = data["RelDen"][0]/PLconstr
+        PCS_res_pb = data["PCS_pb"][0]*Rel_f
+        NCS_res_pb = data["NCS_pb"][0]*Rel_f
+        INDDCS_res_cm3_over_s = data["INDDCS_cm3_over_s"][0]*Rel_f**2
 
-    # check constraints (against rescaled parameters)
-    LZallowed_p = (PCS_res_pb <= LZconstr_pb)
-    LZallowed_n = (NCS_res_pb <= LZconstr_pb)
-    LZallowed = (PCS_res_pb <= LZconstr_pb) and (NCS_res_pb <= LZconstr_pb)
-    FERMIallowed_bb = (INDDCS_res_cm3_over_s*data["INDDCS_bb"][0] <= FERMIconstr_bb or np.isnan(data["INDDCS_bb"][0]))
-    FERMIallowed_tautau = (INDDCS_res_cm3_over_s*data["INDDCS_tautau"][0] <= FERMIconstr_tautau or np.isnan(data["INDDCS_tautau"][0]))
-    FERMIallowed_WW = (INDDCS_res_cm3_over_s*data["INDDCS_WW"][0] <= FERMIconstr_WW or np.isnan(data["INDDCS_WW"][0]))
-    FERMIallowed_cc = (INDDCS_res_cm3_over_s*data["INDDCS_cc"][0] <= FERMIconstr_cc or np.isnan(data["INDDCS_cc"][0]))
-    FERMIallowed_ee = (INDDCS_res_cm3_over_s*data["INDDCS_ee"][0] <= FERMIconstr_ee or np.isnan(data["INDDCS_ee"][0]))
-    FERMIallowed_yy = (INDDCS_res_cm3_over_s*data["INDDCS_yy"][0] <= FERMIconstr_yy or np.isnan(data["INDDCS_yy"][0]))
-    FERMIallowed_gg = (INDDCS_res_cm3_over_s*data["INDDCS_gg"][0] <= FERMIconstr_gg or np.isnan(data["INDDCS_gg"][0]))
-    FERMIallowed_hh = (INDDCS_res_cm3_over_s*data["INDDCS_h2h2"][0] <= FERMIconstr_hh or np.isnan(data["INDDCS_h2h2"][0]))
-    FERMIallowed_mumu = (INDDCS_res_cm3_over_s*data["INDDCS_mumu"][0] <= FERMIconstr_mumu or np.isnan(data["INDDCS_mumu"][0]))
-    FERMIallowed_ss = (INDDCS_res_cm3_over_s*data["INDDCS_ss"][0] <= FERMIconstr_qq or np.isnan(data["INDDCS_ss"][0]))
-    FERMIallowed_dd = (INDDCS_res_cm3_over_s*data["INDDCS_dd"][0] <= FERMIconstr_qq or np.isnan(data["INDDCS_dd"][0]))
-    FERMIallowed_uu = (INDDCS_res_cm3_over_s*data["INDDCS_uu"][0] <= FERMIconstr_qq or np.isnan(data["INDDCS_uu"][0]))
-    FERMIallowed_tt = (INDDCS_res_cm3_over_s*data["INDDCS_tt"][0] <= FERMIconstr_tt or np.isnan(data["INDDCS_tt"][0]))
-    FERMIallowed_ZZ = (INDDCS_res_cm3_over_s*data["INDDCS_ZZ"][0] <= FERMIconstr_ZZ or np.isnan(data["INDDCS_ZZ"][0]))
-    FERMIallowed = (int(FERMIallowed_bb) and int(FERMIallowed_tautau) \
-              and int(FERMIallowed_WW) and int(FERMIallowed_cc) \
-              and int(FERMIallowed_ee) and int(FERMIallowed_yy) \
-              and int(FERMIallowed_gg) and int(FERMIallowed_hh) \
-              and int(FERMIallowed_mumu) and int(FERMIallowed_ss) \
-              and int(FERMIallowed_dd) and int(FERMIallowed_uu) \
-              and int(FERMIallowed_tt) and int(FERMIallowed_ZZ))
-    PLallowed = (data["RelDen"][0] <= PLconstr)
-    # check if allowed by all constraints
-    all_allowed = (data["bfb"][0] and data["unitarity"][0] and int(hbResult.allowed) \
+        # check constraints (against rescaled parameters)
+        LZallowed_p = (PCS_res_pb <= LZconstr_pb)
+        LZallowed_n = (NCS_res_pb <= LZconstr_pb)
+        LZallowed = (PCS_res_pb <= LZconstr_pb) and (NCS_res_pb <= LZconstr_pb)
+        FERMIallowed_bb = (INDDCS_res_cm3_over_s*data["INDDCS_bb"][0] <= FERMIconstr_bb or np.isnan(data["INDDCS_bb"][0]))
+        FERMIallowed_tautau = (INDDCS_res_cm3_over_s*data["INDDCS_tautau"][0] <= FERMIconstr_tautau or np.isnan(data["INDDCS_tautau"][0]))
+        FERMIallowed_WW = (INDDCS_res_cm3_over_s*data["INDDCS_WW"][0] <= FERMIconstr_WW or np.isnan(data["INDDCS_WW"][0]))
+        FERMIallowed_cc = (INDDCS_res_cm3_over_s*data["INDDCS_cc"][0] <= FERMIconstr_cc or np.isnan(data["INDDCS_cc"][0]))
+        FERMIallowed_ee = (INDDCS_res_cm3_over_s*data["INDDCS_ee"][0] <= FERMIconstr_ee or np.isnan(data["INDDCS_ee"][0]))
+        FERMIallowed_yy = (INDDCS_res_cm3_over_s*data["INDDCS_yy"][0] <= FERMIconstr_yy or np.isnan(data["INDDCS_yy"][0]))
+        FERMIallowed_gg = (INDDCS_res_cm3_over_s*data["INDDCS_gg"][0] <= FERMIconstr_gg or np.isnan(data["INDDCS_gg"][0]))
+        FERMIallowed_hh = (INDDCS_res_cm3_over_s*data["INDDCS_h2h2"][0] <= FERMIconstr_hh or np.isnan(data["INDDCS_h2h2"][0]))
+        FERMIallowed_mumu = (INDDCS_res_cm3_over_s*data["INDDCS_mumu"][0] <= FERMIconstr_mumu or np.isnan(data["INDDCS_mumu"][0]))
+        FERMIallowed_ss = (INDDCS_res_cm3_over_s*data["INDDCS_ss"][0] <= FERMIconstr_qq or np.isnan(data["INDDCS_ss"][0]))
+        FERMIallowed_dd = (INDDCS_res_cm3_over_s*data["INDDCS_dd"][0] <= FERMIconstr_qq or np.isnan(data["INDDCS_dd"][0]))
+        FERMIallowed_uu = (INDDCS_res_cm3_over_s*data["INDDCS_uu"][0] <= FERMIconstr_qq or np.isnan(data["INDDCS_uu"][0]))
+        FERMIallowed_tt = (INDDCS_res_cm3_over_s*data["INDDCS_tt"][0] <= FERMIconstr_tt or np.isnan(data["INDDCS_tt"][0]))
+        FERMIallowed_ZZ = (INDDCS_res_cm3_over_s*data["INDDCS_ZZ"][0] <= FERMIconstr_ZZ or np.isnan(data["INDDCS_ZZ"][0]))
+        FERMIallowed = (int(FERMIallowed_bb) and int(FERMIallowed_tautau) \
+                and int(FERMIallowed_WW) and int(FERMIallowed_cc) \
+                and int(FERMIallowed_ee) and int(FERMIallowed_yy) \
+                and int(FERMIallowed_gg) and int(FERMIallowed_hh) \
+                and int(FERMIallowed_mumu) and int(FERMIallowed_ss) \
+                and int(FERMIallowed_dd) and int(FERMIallowed_uu) \
+                and int(FERMIallowed_tt) and int(FERMIallowed_ZZ))
+        PLallowed = (data["RelDen"][0] <= PLconstr)
+        # check if allowed by all constraints
+        all_allowed = (data["bfb"][0] and data["unitarity"][0] and int(hbResult.allowed) \
               and int(LZallowed) and int(FERMIallowed) and int(PLallowed))
-    # put results into one dictionary
-    results = {'HBallowed': [int(hbResult.allowed)],
-               'Chisq': [hsChisq],
-               'Chisq_red': [hsChisq_red], 
-               'Chisq_CMS-LEP': [Chisq_CMS_LEP],
-               'mu_the_LEP': [mu_the_LEP], 
-               'mu_the_CMS': [mu_the_CMS],
-               'Planckallowed': [int(PLallowed)], 
-               'Planckconstr': PLconstr,
-               'LZallowed': [int(LZallowed)],
-               'LZallowed_p': [int(LZallowed_p)], 
-               'LZallowed_n': [int(LZallowed_n)],
-               'LZconstr_pb': [LZconstr_pb],
-               'FERMIallowed': [int(FERMIallowed)],
-               'FERMIallowed_bb': [int(FERMIallowed_bb)], 
-               'FERMIconstr_bb': [FERMIconstr_bb],
-               'FERMIallowed_tautau': [int(FERMIallowed_tautau)], 
-               'FERMIconstr_tautau': [FERMIconstr_tautau],
-               'FERMIallowed_WW': [int(FERMIallowed_WW)], 
-               'FERMIconstr_WW': [FERMIconstr_WW],
-               'FERMIallowed_cc': [int(FERMIallowed_cc)], 
-               'FERMIconstr_cc': [FERMIconstr_cc],
-               'FERMIallowed_ee': [int(FERMIallowed_ee)], 
-               'FERMIconstr_ee': [FERMIconstr_ee],
-               'FERMIallowed_yy': [int(FERMIallowed_yy)], 
-               'FERMIconstr_yy': [FERMIconstr_yy],
-               'FERMIallowed_gg': [int(FERMIallowed_gg)], 
-               'FERMIconstr_gg': [FERMIconstr_gg],
-               'FERMIallowed_hh': [int(FERMIallowed_hh)], 
-               'FERMIconstr_hh': [FERMIconstr_hh],
-               'FERMIallowed_mumu': [int(FERMIallowed_mumu)], 
-               'FERMIconstr_mumu': [FERMIconstr_mumu],
-               'FERMIallowed_ss': [int(FERMIallowed_ss)], 
-               'FERMIconstr_ss/qq': [FERMIconstr_qq],
-               'FERMIallowed_dd': [int(FERMIallowed_dd)], 
-               'FERMIconstr_dd/qq': [FERMIconstr_qq],
-               'FERMIallowed_uu': [int(FERMIallowed_uu)], 
-               'FERMIconstr_uu/qq': [FERMIconstr_qq],
-               'FERMIallowed_tt': [int(FERMIallowed_tt)], 
-               'FERMIconstr_tt': [FERMIconstr_tt],
-               'FERMIallowed_ZZ': [int(FERMIallowed_ZZ)], 
-               'FERMIconstr_ZZ': [FERMIconstr_ZZ],
-               'Rel_f': [Rel_f], 
-               'PCS_res_pb': [PCS_res_pb], 
-               'NCS_res_pb': [NCS_res_pb], 
-               'INDDCS_res_cm3_over_s': [INDDCS_res_cm3_over_s],
-               'allallowed': [int(all_allowed)]}
+        # put results into one dictionary
+        results = {'HBallowed': [int(HBallowed)],
+                'Chisq': [hsChisq],
+                'Chisq_red': [hsChisq_red], 
+                'Chisq_CMS-LEP': [Chisq_CMS_LEP],
+                'mu_the_LEP': [mu_the_LEP], 
+                'mu_the_CMS': [mu_the_CMS],
+                'Planckallowed': [int(PLallowed)], 
+                'Planckconstr': PLconstr,
+                'LZallowed': [int(LZallowed)],
+                'LZallowed_p': [int(LZallowed_p)], 
+                'LZallowed_n': [int(LZallowed_n)],
+                'LZconstr_pb': [LZconstr_pb],
+                'FERMIallowed': [int(FERMIallowed)],
+                'FERMIallowed_bb': [int(FERMIallowed_bb)], 
+                'FERMIconstr_bb': [FERMIconstr_bb],
+                'FERMIallowed_tautau': [int(FERMIallowed_tautau)], 
+                'FERMIconstr_tautau': [FERMIconstr_tautau],
+                'FERMIallowed_WW': [int(FERMIallowed_WW)], 
+                'FERMIconstr_WW': [FERMIconstr_WW],
+                'FERMIallowed_cc': [int(FERMIallowed_cc)], 
+                'FERMIconstr_cc': [FERMIconstr_cc],
+                'FERMIallowed_ee': [int(FERMIallowed_ee)], 
+                'FERMIconstr_ee': [FERMIconstr_ee],
+                'FERMIallowed_yy': [int(FERMIallowed_yy)], 
+                'FERMIconstr_yy': [FERMIconstr_yy],
+                'FERMIallowed_gg': [int(FERMIallowed_gg)], 
+                'FERMIconstr_gg': [FERMIconstr_gg],
+                'FERMIallowed_hh': [int(FERMIallowed_hh)], 
+                'FERMIconstr_hh': [FERMIconstr_hh],
+                'FERMIallowed_mumu': [int(FERMIallowed_mumu)], 
+                'FERMIconstr_mumu': [FERMIconstr_mumu],
+                'FERMIallowed_ss': [int(FERMIallowed_ss)], 
+                'FERMIconstr_ss/qq': [FERMIconstr_qq],
+                'FERMIallowed_dd': [int(FERMIallowed_dd)], 
+                'FERMIconstr_dd/qq': [FERMIconstr_qq],
+                'FERMIallowed_uu': [int(FERMIallowed_uu)], 
+                'FERMIconstr_uu/qq': [FERMIconstr_qq],
+                'FERMIallowed_tt': [int(FERMIallowed_tt)], 
+                'FERMIconstr_tt': [FERMIconstr_tt],
+                'FERMIallowed_ZZ': [int(FERMIallowed_ZZ)], 
+                'FERMIconstr_ZZ': [FERMIconstr_ZZ],
+                'Rel_f': [Rel_f], 
+                'PCS_res_pb': [PCS_res_pb], 
+                'NCS_res_pb': [NCS_res_pb], 
+                'INDDCS_res_cm3_over_s': [INDDCS_res_cm3_over_s],
+                'allallowed': [int(all_allowed)]}
+    except:
+        hbResult = np.nan
+        results = {'HBallowed': [np.nan],
+                'Chisq': [np.nan],
+                'Chisq_red': [np.nan], 
+                'Chisq_CMS-LEP': [np.nan],
+                'mu_the_LEP': [np.nan], 
+                'mu_the_CMS': [np.nan],
+                'Planckallowed': [np.nan], 
+                'Planckconstr': np.nan,
+                'LZallowed': [np.nan],
+                'LZallowed_p': [np.nan], 
+                'LZallowed_n': [np.nan],
+                'LZconstr_pb': [np.nan],
+                'FERMIallowed': [np.nan],
+                'FERMIallowed_bb': [np.nan], 
+                'FERMIconstr_bb': [np.nan],
+                'FERMIallowed_tautau': [np.nan], 
+                'FERMIconstr_tautau': [np.nan],
+                'FERMIallowed_WW': [np.nan], 
+                'FERMIconstr_WW': [np.nan],
+                'FERMIallowed_cc': [np.nan], 
+                'FERMIconstr_cc': [np.nan],
+                'FERMIallowed_ee': [np.nan], 
+                'FERMIconstr_ee': [np.nan],
+                'FERMIallowed_yy': [np.nan], 
+                'FERMIconstr_yy': [np.nan],
+                'FERMIallowed_gg': [np.nan], 
+                'FERMIconstr_gg': [np.nan],
+                'FERMIallowed_hh': [np.nan], 
+                'FERMIconstr_hh': [np.nan],
+                'FERMIallowed_mumu': [np.nan], 
+                'FERMIconstr_mumu': [np.nan],
+                'FERMIallowed_ss': [np.nan], 
+                'FERMIconstr_ss/qq': [np.nan],
+                'FERMIallowed_dd': [np.nan], 
+                'FERMIconstr_dd/qq': [np.nan],
+                'FERMIallowed_uu': [np.nan], 
+                'FERMIconstr_uu/qq': [np.nan],
+                'FERMIallowed_tt': [np.nan], 
+                'FERMIconstr_tt': [np.nan],
+                'FERMIallowed_ZZ': [np.nan], 
+                'FERMIconstr_ZZ': [np.nan],
+                'Rel_f': [np.nan], 
+                'PCS_res_pb': [np.nan], 
+                'NCS_res_pb': [np.nan], 
+                'INDDCS_res_cm3_over_s': [np.nan],
+                'allallowed': [0]}
     return results, hbResult
 
 def get_higgstools(data):
@@ -160,7 +211,7 @@ def get_higgstools(data):
     signals = HS.Signals(data["HS_DIR"][0])
     OUT=hinput.readHB5SLHA(HT_INP, neutralIds, chargedIds)
     PRED=hinput.predictionsFromDict(OUT, neutralIds2, chargedIds2,
-                                    doublechargedIds2)
+                                doublechargedIds2)
     hbResult = bounds(PRED)
     hsChisq = signals(PRED)
     hsChisq_red = hsChisq / signals.observableCount()
@@ -193,14 +244,19 @@ def calc_chisq_cms_lep(data):
     mu_exp_LEP = 0.117
     sigma_mu_exp_LEP = 0.057
     mu_exp_ATLAS_CMS = 0.24
-    sigma_mu_exp_ATLAS_CMS = 0.09
+    sigma_mu_exp_ATLAS_CMS_upper = 0.09
+    sigma_mu_exp_ATLAS_CMS_lower = 0.08
     # calculate predicted 2HDMS signal strengths
     mu_the_LEP = c_h1VV_sq * BR_h1bb / BR_SM_Hbb
     mu_the_CMS = c_h1tt_sq * BR_h1yy / BR_SM_Hyy
 
     # calculate Chisq_{CMS-LEP}
-    chisq = ((mu_the_LEP - mu_exp_LEP)/sigma_mu_exp_LEP)**2 + \
-            ((mu_the_CMS - mu_exp_ATLAS_CMS)/sigma_mu_exp_ATLAS_CMS)**2
+    if mu_the_CMS >= mu_exp_ATLAS_CMS:
+        chisq = ((mu_the_LEP - mu_exp_LEP)/sigma_mu_exp_LEP)**2 + \
+                ((mu_the_CMS - mu_exp_ATLAS_CMS)/sigma_mu_exp_ATLAS_CMS_upper)**2
+    else:
+        chisq = ((mu_the_LEP - mu_exp_LEP)/sigma_mu_exp_LEP)**2 + \
+                ((mu_the_CMS - mu_exp_ATLAS_CMS)/sigma_mu_exp_ATLAS_CMS_lower)**2
     return chisq, mu_the_LEP, mu_the_CMS
 
 def get_interp_constr(DM_mass, FILE):

PlotResults_rescaled_NEW.py
@@ -46,14 +46,15 @@ labels_dict = {"dl14p": "$\delta_{14}'$",
                "INDDCS_tt": "$\sigma_{A_S A_S → t t} \, [cm^3/s]$",
                "INDDCS_tautau": "$\sigma_{A_S A_S → τ τ} \, [cm^3/s]$",
                "INDDCS_WW": "$\sigma_{A_S A_S → W W} \, [cm^3/s]$",
+               "INDDCS_h1h1": "$\sigma_{A_S A_S → h_1 h_1} \, [cm^3/s]$",
                "INDDCS_h2h2": "$\sigma_{A_S A_S → h_2 h_2} \, [cm^3/s]$",
                "INDDCS_h1h2": "$\sigma_{A_S A_S → h_1 h_2} \, [cm^3/s]$",
                "INDDCS_hihj": "$\sum_{i,j} \sigma_{A_S A_S → h_i h_j} \, [cm^3/s]$",
                "mu_the_LEP": "$\mu_{LEP}$",
                "mu_the_CMS": "$\mu_{CMS}$",
-               "l1m24p": "$\lambda_1' - 2\lambda_4'$",
-               "l2m25p": "$\lambda_2' - 2\lambda_5'$",
-               "l1ml3pp": "$\lambda_1'' - \lambda_3''$",
+               "l1m24p": "$\lambda_{14}' = \lambda_1' - 2\lambda_4'$",
+               "l2m25p": "$\lambda_{25}' = \lambda_2' - 2\lambda_5'$",
+               "l1ml3pp": "$\lambda_{13}'' = \lambda_1'' - \lambda_3''$",
                "l1": "$\lambda_{1}$",
                "l2": "$\lambda_{2}$",
                "l3": "$\lambda_{3}$",
@@ -97,9 +98,12 @@ file_out_name_dict = {"RelDen": "RelDen",
                "lh1ss_norm": "lh1",
                "lh2ss_norm": "lh2",
                "lh3ss_norm": "lh3",
-               "INDDCS_bb": "InddCSbb", "INDDCS_tt": "InddCStt",
+               "INDDCS_bb": "InddCSbb", 
+               "INDDCS_tt": "InddCStt",
                "INDDCS_tautau": "InddCStautau",
-               "INDDCS_WW": "InddCSWW", "INDDCS_h2h2": "InddCShh",
+               "INDDCS_WW": "InddCSWW", 
+               "INDDCS_h1h1": "InddCSh1h1",
+               "INDDCS_h2h2": "InddCShh",
                "INDDCS_h1h2": "InddCSh1h2",
                "INDDCS_hihj": "InddCShihj"}
 legend_hatch_dict = {"/": "////",
@@ -134,7 +138,7 @@ def plot_all(data, PATH):
     # plot the data
 
     plot_1(XPARAM, YPARAM, "RelDen", tick_length, tick_space, line_space, data,
-           shape, PATH, matplotlib.colors.LogNorm(), None)
+           shape, PATH, matplotlib.colors.LogNorm(vmin=np.nanmin(data["RelDen"]), vmax=np.nanmax([np.nanmax(data["RelDen"]), 0.13])), None)
     #plot_1(XPARAM, YPARAM, "INDDCS_hihj", tick_length, tick_space, line_space, data,
     #       shape, PATH, matplotlib.colors.LogNorm(), None)
     plot_1(XPARAM, YPARAM, "BR_h3SS", tick_length, tick_space, line_space, data,
@@ -148,6 +152,8 @@ def plot_all(data, PATH):
     plot_3(XPARAM, YPARAM, "INDDCS_h2h2", "INDDCS_WW", "INDDCS_bb", tick_length, tick_space,
            line_space, data, shape, PATH, matplotlib.colors.LogNorm(), None)
     plot_3(XPARAM, YPARAM, "INDDCS_bb", "INDDCS_tt", "INDDCS_h1h2", tick_length, tick_space,
+           line_space, data, shape, PATH, matplotlib.colors.LogNorm(), None)
+    plot_3(XPARAM, YPARAM, "INDDCS_bb", "INDDCS_WW", "INDDCS_h1h1", tick_length, tick_space,
            line_space, data, shape, PATH, matplotlib.colors.LogNorm(), None)
     #plot_3(XPARAM, YPARAM, "INDDCS_bb", "INDDCS_INDDCS_tautau", "INDDCS_WW",
     #       tick_length, tick_space, line_space, data, shape, PATH, None, MagnitudeFormatter(-25))
@@ -192,6 +198,7 @@ def get_shape(START_VAL, STOP_VAL, STEP_SIZE, START_VAL2, STOP_VAL2, STEP_SIZE2)
     X=np.floor(1 + (STOP_VAL-START_VAL)/STEP_SIZE)
     Y=np.floor(1 + (STOP_VAL2-START_VAL2)/STEP_SIZE2)
     shape = (int(X),int(Y))
+    shape = (55,51)
     return shape
 
 def get_factor(PARAM, data, shape):
@@ -199,7 +206,7 @@ def get_factor(PARAM, data, shape):
         FACTOR = 1e-36 * np.array(data["Rel_f"]).reshape(shape)
     elif (PARAM == "INDDCS_bb" or PARAM == "INDDCS_tt" or PARAM == "INDDCS_tautau"
           or PARAM == "INDDCS_WW" or PARAM == "INDDCS_h2h2" or PARAM == "INDDCS_h1h2"
-          or PARAM == "INDDCS_hihj"):
+          or PARAM == "INDDCS_hihj" or PARAM == "INDDCS_h1h1"):
         FACTOR = np.array(data["INDDCS_res_cm3_over_s"]).reshape(shape)
     else:
         FACTOR = 1
@@ -325,8 +332,10 @@ def plot_bp(XPARAM, YPARAM, ZPARAM, ax, ps):
     #BP_FILE = "results.csv"
     #BP_PATH = "~/SyncandShare/Master/FILES/benchmark_points/new_BPs/BP3_95.4_3x700"
     #BP_FILE = "BP3_95.4_3x700_new_notation.csv"
-    BP_PATH = "~/SyncandShare/2HDMS/FILES/benchmark_points/mucoll_BP2_new_basis"
-    BP_FILE = "results_mAS-tanbeta.csv"
+    #BP_PATH = "~/SyncandShare/2HDMS/FILES/benchmark_points/mucoll_BP2_new_basis"
+    #BP_FILE = "results_mAS-tanbeta.csv"
+    BP_PATH = "~/SyncandShare/2HDMS-Z2breaking_mucoll_paper/benchmark_points/mucoll_BP3"
+    BP_FILE = "results_BP3_newbasis.csv"
     BP_data=pd.read_csv(BP_PATH+"/"+BP_FILE)
     #ZFACTOR = get_factor(ZPARAM, BP_data, (1))
     if XPARAM == "m122":
@@ -346,14 +355,16 @@ def plot_bp(XPARAM, YPARAM, ZPARAM, ax, ps):
 
 def make_subplot(ax, X, Y, Z, bfb, unitarity, HB, ZPARAM, data, zlabel, shape,
                  tick_length, tick_space, line_space, fig, XPARAM, YPARAM, norm, ax_multipl):
-    ps = 45
+    ps = 50
     if np.isnan(Z).all()==False:
         # if the values does not change, set some arbitrary limits for the axes
         if (np.max(Z)-np.min(Z)) == 0:
             pos=ax.scatter(X, Y, s=ps, c=Z, vmin=np.max(Z)-1, vmax=np.max(Z)+1)
         # if plotting relic density make sure that Planck limit is covered by the axis
-        elif ZPARAM == 'RelDen':
-            pos=ax.scatter(X, Y, s=ps, c=Z, vmin=np.min(Z), vmax=np.max([np.max(Z), 0.1201]))
+        # removed this part again because we can not use lognorm with it
+        # the limits are set in the main plotting function
+        #elif ZPARAM == 'RelDen':
+        #    pos=ax.scatter(X, Y, s=ps, c=Z, vmin=np.nanmin(Z), vmax=np.nanmax([np.max(Z), 0.1201]))
         # or just set the limits automatically
         else:
             pos=ax.scatter(X, Y, s=ps, c=Z, norm=norm)
@@ -369,8 +380,29 @@ def make_subplot(ax, X, Y, Z, bfb, unitarity, HB, ZPARAM, data, zlabel, shape,
             add_constr_data = np.array(data[add_constr_name]).reshape(shape)
             circ4 = plot_constr(X, Y, add_constr_data, add_constr_name, "solid",
                                                 tick_length, tick_space, line_space, ax, "..")
+        # make second x axis to show mass of other particles (if mAS is on x axis)
+        # NOTE: this part is not finished to work automatically for every
+        #       possible setup, it might have to be adjusted by hand
+        #if XPARAM=='mAS':
+        #    if data['mh3'][0]==data['mHm'][0] and data['mh3'][0]==data['mA'][0]:
+        #        def linear_func(x):
+        #            return x
+        #        ax2 = ax.secondary_xaxis('top', functions=(linear_func, linear_func))
+                #ax2.set_xlabel('$m$ [GeV]')
+        #        ticks = [data['mh2'][0]/2, data['mh1'][0], data['mh2'][0], data['mh3'][0]/2]
+                #labels = [r'$\frac{m_{h_2}}{2}$     ', '$m_{h_1}$', '     $m_{h_2}$', r'$\frac{m_{h_3}, m_{A}, m_{H^\pm}}{2}$']
+        #        labels = [r'$\frac{m_{h2}}{2}$    ', '$m_{h1}$', '     $m_{h2}$', r'$\frac{m_{h3}, m_{A}, m_{H\pm}}{2}$']
+                #labels = ['$m_{h2}/2$        ', '$m_{h1}$', '     $m_{h2}$', '$(m_{h3}, m_{A}, m_{H\pm})/2$']
+        #        ax2.set_xticks(ticks=ticks, labels=labels, fontsize=12)
+
+                #arrowprops = dict(arrowstyle="-",
+                #              connectionstyle="angle,angleA=0,angleB=90,rad=10")
+                #ax.annotate('$m_{h_2}/2$', (data['mh2'][0]/2,20), (data['mh2'][0]/2-50,22), arrowprops=arrowprops)
+                #ax.annotate('$m_{h_1}$', (data['mh1'][0],20), (data['mh1'][0]-20,23), arrowprops=arrowprops)
+                #ax.annotate('$m_{h_2}$', (data['mh2'][0],20), (data['mh2'][0]-20,22), arrowprops=arrowprops)
+                #ax.annotate('$(m_{h_3}, m_{A}, m_{H^\pm})/2$', (data['mh3'][0]/2,20), (data['mh3'][0]/2-50,23), arrowprops=arrowprops)
         # plot BP
-        #plot_bp(XPARAM, YPARAM, ZPARAM, ax, ps)
+        plot_bp(XPARAM, YPARAM, ZPARAM, ax, ps)
         # make legend
         if ZPARAM in constr_dict.keys():
             circ_o = [circ1, circ2, circ3, circ4]
@@ -385,9 +417,21 @@ def make_subplot(ax, X, Y, Z, bfb, unitarity, HB, ZPARAM, data, zlabel, shape,
         # if plotting relic density make a red mark on the color bar at 0.1191 (desired relic density)
         if ZPARAM == 'RelDen':
             bar.ax.axhspan(0.1181, 0.1201, color='red') # 0.1191 +- 0.001
+            # NOTE the label for the Planck limit needs to be adjusted by hand
+            # one option is to add the labela s text
+            plt.text(np.nanmax(X)-20 , np.nanmax(Y)-0,'Planck\nlimit', color='red', fontsize=8)
+            # another option is to add it to the ticks
+            #bar_ticks = bar.get_ticks()
+            #bar_labels = bar_ticks
+            #print(bar_ticks)
+            #new_ticks = np.append(bar_ticks[2:-2], 1.191e-01)
+            #print(new_ticks)
+            #print([bar_ticks, 0,19])
+            #new_labels = np.append(bar_ticks[2:-2], 'Planck\nlimit')
+            #bar.set_ticks(ticks=bar_ticks, labels=bar_ticks)
         # set limis and formatting for axes
         #ax.set_xlim(0,1)
-        #ax.set_xlim(100,400)
+        ax.set_xlim(np.nanmin(X),600)
         #ax.set_ylim(7,11)
         #ax.set_ylim(-60000,20000)
         #ax.yaxis.set_major_formatter(MagnitudeFormatter(4))
@@ -420,6 +464,17 @@ def plot_1(XPARAM, YPARAM, ZPARAM, tick_length, tick_space, line_space, data, sh
     fig, ax = plt.subplots()
     circ = make_subplot(ax, X, Y, Z, bfb, unitarity, HB, ZPARAM, data, zlabel, shape,
                  tick_length, tick_space, line_space, fig, XPARAM, YPARAM, norm, ax_multipl)
+    if XPARAM=='mAS':
+        if data['mh3'][0]==data['mHm'][0] and data['mh3'][0]==data['mA'][0]:
+            def linear_func(x):
+                return x
+            axsec = ax.secondary_xaxis('top', functions=(linear_func, linear_func))
+            #ax2.set_xlabel('$m$ [GeV]')
+            ticks = [data['mh2'][0]/2, data['mh1'][0], data['mh2'][0], data['mh3'][0]/2]
+            #labels = [r'$\frac{m_{h_2}}{2}$     ', '$m_{h_1}$', '     $m_{h_2}$', r'$\frac{m_{h_3}, m_{A}, m_{H^\pm}}{2}$']
+            labels = [r'$\frac{m_{h2}}{2}$    ', '$m_{h1}$', '     $m_{h2}$', r'$\frac{m_{h3}, m_{A}, m_{H\pm}}{2}$']
+            #labels = ['$m_{h2}/2$        ', '$m_{h1}$', '     $m_{h2}$', '$(m_{h3}, m_{A}, m_{H\pm})/2$']
+            axsec.set_xticks(ticks=ticks, labels=labels, fontsize=12)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
     ax.legend(handles=circ, loc="upper right", framealpha=1)
@@ -463,6 +518,17 @@ def plot_2(XPARAM, YPARAM, ZPARAM1, ZPARAM2, tick_length, tick_space, line_space
                  tick_length, tick_space, line_space, fig, XPARAM, YPARAM, norm, ax_multipl)
     circ2 = make_subplot(ax2, X, Y, Z2, bfb, unitarity, HB, ZPARAM2, data, zlabel2, shape,
                  tick_length, tick_space, line_space, fig, XPARAM, YPARAM, norm, ax_multipl)
+    if XPARAM=='mAS':
+        if data['mh3'][0]==data['mHm'][0] and data['mh3'][0]==data['mA'][0]:
+            def linear_func(x):
+                return x
+            axsec = ax1.secondary_xaxis('top', functions=(linear_func, linear_func))
+            #ax2.set_xlabel('$m$ [GeV]')
+            ticks = [data['mh2'][0]/2, data['mh1'][0], data['mh2'][0], data['mh3'][0]/2]
+            #labels = [r'$\frac{m_{h_2}}{2}$     ', '$m_{h_1}$', '     $m_{h_2}$', r'$\frac{m_{h_3}, m_{A}, m_{H^\pm}}{2}$']
+            labels = [r'$\frac{m_{h2}}{2}$    ', '$m_{h1}$', '     $m_{h2}$', r'$\frac{m_{h3}, m_{A}, m_{H\pm}}{2}$']
+            #labels = ['$m_{h2}/2$        ', '$m_{h1}$', '     $m_{h2}$', '$(m_{h3}, m_{A}, m_{H\pm})/2$']
+            axsec.set_xticks(ticks=ticks, labels=labels, fontsize=12)
     ax2.set_xlabel(xlabel)
     ax1.set_ylabel(ylabel)
     ax2.set_ylabel(ylabel)
@@ -519,6 +585,17 @@ def plot_3(XPARAM, YPARAM, ZPARAM1, ZPARAM2, ZPARAM3, tick_length,
                  tick_length, tick_space, line_space, fig, XPARAM, YPARAM, norm, ax_multipl)
     circ3 = make_subplot(ax3, X, Y, Z3, bfb, unitarity, HB, ZPARAM3, data, zlabel3, shape,
                  tick_length, tick_space, line_space, fig, XPARAM, YPARAM, norm, ax_multipl)
+    if XPARAM=='mAS':
+        if data['mh3'][0]==data['mHm'][0] and data['mh3'][0]==data['mA'][0]:
+            def linear_func(x):
+                return x
+            axsec = ax1.secondary_xaxis('top', functions=(linear_func, linear_func))
+            #ax2.set_xlabel('$m$ [GeV]')
+            ticks = [data['mh2'][0]/2, data['mh1'][0], data['mh2'][0], data['mh3'][0]/2]
+            #labels = [r'$\frac{m_{h_2}}{2}$     ', '$m_{h_1}$', '     $m_{h_2}$', r'$\frac{m_{h_3}, m_{A}, m_{H^\pm}}{2}$']
+            labels = [r'$\frac{m_{h2}}{2}$    ', '$m_{h1}$', '     $m_{h2}$', r'$\frac{m_{h3}, m_{A}, m_{H\pm}}{2}$']
+            #labels = ['$m_{h2}/2$        ', '$m_{h1}$', '     $m_{h2}$', '$(m_{h3}, m_{A}, m_{H\pm})/2$']
+            axsec.set_xticks(ticks=ticks, labels=labels, fontsize=12)
     ax3.set_xlabel(xlabel)
     ax2.set_ylabel(ylabel)
     ax1.legend(handles=circ1, loc="upper right", framealpha=1)
@@ -557,7 +634,7 @@ def plot_all_constr_s1(XPARAM, YPARAM, tick_length, tick_space,
     circ6 = plot_constr(X, Y, fermi, "FERMIallowed", "solid", tick_length,
                                         tick_space, line_space, ax, "o")
     # plot BP
-    #plot_bp(XPARAM, YPARAM, None, ax, None)
+    plot_bp(XPARAM, YPARAM, None, ax, None)
     # make legend
     circ_o = [circ0, circ1, circ2, circ3, circ4,
                circ5, circ6]
@@ -605,8 +682,19 @@ def plot_all_constr_s2(XPARAM, YPARAM, tick_length, tick_space,
                                         tick_space, line_space, ax, "-", color="gold")
     circ6 = plot_constr(X, Y, fermi, "FERMIallowed", "solid", tick_length,
                                         tick_space, line_space, ax, "\\", color="darkturquoise")
+    if XPARAM=='mAS':
+        if data['mh3'][0]==data['mHm'][0] and data['mh3'][0]==data['mA'][0]:
+            def linear_func(x):
+                return x
+            axsec = ax.secondary_xaxis('top', functions=(linear_func, linear_func))
+            #ax2.set_xlabel('$m$ [GeV]')
+            ticks = [data['mh2'][0]/2, data['mh1'][0], data['mh2'][0], data['mh3'][0]/2]
+            #labels = [r'$\frac{m_{h_2}}{2}$     ', '$m_{h_1}$', '     $m_{h_2}$', r'$\frac{m_{h_3}, m_{A}, m_{H^\pm}}{2}$']
+            labels = [r'$\frac{m_{h2}}{2}$    ', '$m_{h1}$', '     $m_{h2}$', r'$\frac{m_{h3}, m_{A}, m_{H\pm}}{2}$']
+            #labels = ['$m_{h2}/2$        ', '$m_{h1}$', '     $m_{h2}$', '$(m_{h3}, m_{A}, m_{H\pm})/2$']
+            axsec.set_xticks(ticks=ticks, labels=labels, fontsize=12)
     # plot BP
-    #plot_bp(XPARAM, YPARAM, None, ax, None)
+    plot_bp(XPARAM, YPARAM, None, ax, None)
     # make legend
     circ_o = [circ0, circ1, circ2, circ3, circ4,
                circ5, circ6]
@@ -659,7 +747,7 @@ def plot_all_constr_s3(XPARAM, YPARAM, tick_length, tick_space,
     circ6 = plot_constr_s3(X, Y, fermi, "FERMIallowed", "dotted", tick_length,
                                         tick_space, line_space, ax, "O", color="dodgerblue", alpha=0.2)
     # plot BP
-    #plot_bp(XPARAM, YPARAM, None, ax, None)
+    plot_bp(XPARAM, YPARAM, None, ax, None)
     # make legend
     circ_o = [circ0, circ1, circ2, circ3, circ4,
                circ5, circ6]